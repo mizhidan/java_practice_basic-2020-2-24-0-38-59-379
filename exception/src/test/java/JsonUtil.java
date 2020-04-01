5710e9856568e3eb98246ad122e2ad6d50e5964e
mizhidan <dj9824@126.com>
2020-04-01 14:52:17 +0800

    [danjie] feat: first test

--- exception/src/test/java/JsonUtil.java
+++ exception/src/test/java/JsonUtil.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
@@ -9,7 +10,12 @@ public class JsonUtil {
 
   public static String convertToJson(Object object) {
     //TODO: change the code to pass the test
-
-    throw new NotImplementedException();
+    String result = null;
+    try {
+      result = objectMapper.writeValueAsString(object);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+    return result;
   }
 }
